34387e6fd3720f5a3c4f4977ca0b091515b01a6a
Burada.PramodhSai <143316696+buradapramodh@users.noreply.github.com>
2026-02-10 23:03:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,6 +81,10 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
+
+  // ⭐ THIS LINE FIXES MAPBOX
+  res.locals.MAP_TOKEN = process.env.MAP_TOKEN;
+
   next();
 });
 
